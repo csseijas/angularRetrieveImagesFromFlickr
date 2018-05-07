cee374472e5a7a8d073da61fe90dff2c066eafce
csseijas <sanchezseijas85@hotmail.com>
2018-05-07 12:22:27 +0200

    Update flickr.service.ts

--- test-project/src/app/flickr.service.ts
+++ test-project/src/app/flickr.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {Jsonp, Headers, RequestOptions} from '@angular/http';
+import {Jsonp} from '@angular/http';
 @Injectable()
 export class FlickrService {
 
